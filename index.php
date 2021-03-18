bd5f7f1468f74380bdd2856b09dc25f3d76aa29f
daarrr <80875893+daarrr@users.noreply.github.com>
2021-03-18 22:30:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Daar's Community, SERVER RULES">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Home</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.9.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body"><header class="u-align-left u-clearfix u-gradient u-header u-header" id="sec-53b6"><div class="u-clearfix u-sheet u-sheet-1"></div></header>
+    <section class="u-clearfix u-gradient u-section-1" id="sec-9539">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-1" data-animation-name="flipIn" data-animation-duration="2500" data-animation-delay="0" data-animation-direction="X">Daar's Community</h1>
+        <a href="#" class="u-btn u-btn-round u-button-style u-gradient u-none u-radius-18 u-text-body-alt-color u-btn-1" data-animation-name="bounceIn" data-animation-duration="2000" data-animation-delay="0" data-animation-direction="">JOIN NOW</a>
+      </div>
+    </section>
+    <section class="skrollable u-clearfix u-grey-60 u-section-2" id="sec-8576">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-palette-2-base u-text-1" data-animation-name="tada" data-animation-duration="2750" data-animation-delay="0" data-animation-direction="">SERVER RULES</h2>
+        <div class="u-container-style u-expanded-width u-grey-70 u-group u-radius-10 u-shape-round u-group-1" data-animation-name="zoomIn" data-animation-duration="750" data-animation-delay="0" data-animation-direction="">
+          <div class="u-container-layout u-container-layout-1">
+            <h6 class="u-text u-text-grey-5 u-text-2">Do not organize, participate in, or encourage harassment of others.&nbsp;Disagreements happen and are normal, but continuous, repetitive, or severe negative comments may cross the line into harassment and are not okay.<br>Do not organize, promote, or coordinate servers around hate speech.&nbsp;It’s unacceptable to attack a person or a community based on attributes such as their race, ethnicity, national origin, sex, gender, sexual orientation, religious affiliation, or disabilities.<br>Do not make threats of violence or threaten to harm others.&nbsp;This includes indirect threats, as well as sharing or threatening to share someone’s private personal information (also known as doxxing).<br>Do not evade user blocks or server bans.&nbsp;Do not send unwanted, repeated friend requests or messages, especially after they’ve made it clear they don’t want to talk to you anymore. Do not try to hide your identity in an attempt to contact someone who has blocked you, or otherwise circumvent the tools we have which enable users to protect themselves.<br>Do not send others viruses or malware, attempt to phish others, or hack or DDoS them.<br>Here are some rules for content on Discord:You must apply the NSFW label to channels if there is adult content in that channel.&nbsp;Any content that cannot be placed in an age-gated channel, such as avatars, server banners, and invite splashes, may not contain adult content.<br>You may not sexualize minors in any way.&nbsp;This includes sharing content or links which depict minors in a pornographic, sexually suggestive, or violent manner, and includes illustrated or digitally altered pornography that depicts minors (such as lolicon, shotacon, or cub). We report illegal content to the&nbsp;<a href="https://www.missingkids.org/gethelpnow/cybertipline" target="_blank" rel="noreferrer" class="u-active-none u-border-none u-btn u-button-style u-hover-none u-none u-text-palette-1-base u-btn-1">National Center for Missing and Exploited Children</a>.<br>You may not share sexually explicit content of other people without their consent, or share or promote sharing of non-consensual intimate imagery (also known as revenge porn) in an attempt to shame or degrade someone.<br>You may not share content that glorifies or promotes suicide or self-harm, including any encouragement to others to cut themselves, or embrace eating disorders such as anorexia or bulimia.<br>You may not share images of sadistic gore or animal cruelty.<br>You may not use Discord for the organization, promotion, or support of violent extremism.<br>You may not operate a server that sells or facilitates the sales of prohibited or potentially dangerous goods.&nbsp;This includes firearms, ammunition, drugs, and controlled substances.<br>You may not promote, distribute, or provide access to content involving the hacking, cracking, or distribution of pirated software or stolen accounts.&nbsp;This includes sharing or selling cheats or hacks that may negatively affect others in multiplayer games.<br>In general,&nbsp;you should not promote, encourage or engage in any illegal behavior.&nbsp;This is very likely to get you kicked off Discord, and may get you reported to law enforcement.
+            </h6>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-left u-clearfix u-grey-50 u-section-3" id="sec-6c8b">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-3b70"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">This Is NOT AFFILIATED With Discord<br>Made by daar<br>
+          <br>Owned By Daar
+        </p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+    </section>
+  </body>
+</html>
